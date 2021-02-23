54e3fb4a481d86e05e94311bd2f1ef0023c50797
hilman-xd72 <69209970+hilman-maulana72@users.noreply.github.com>
2021-02-23 18:58:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,87 @@
-pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;git pull;python2 cr4ck.so
+#!/bin/bash
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+pip install requests
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip install mechanize
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip install requests bs4
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pkg install figlet
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip2 install requests
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip2 install tqdm
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip2 install mechanize
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pkg install ruby -y
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+gem install lolcat
+clear
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+clear
+figlet Hilman | lolcat
+sleep 2
+clear
+figlet Hilman | lolcat
+sleep 2
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+echo
+git pull
+sleep 2
+clear
+python Z.cpython-39.pyc
